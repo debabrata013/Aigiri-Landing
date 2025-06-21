6d94af59714cfbac68fc12029a318b24a5acdaaf
DEV <pattnaikd833@gmail.com>
2025-06-21 11:20:29 +0530

    chore: update GitHub Actions workflow for Next.js deployment and improve configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  images: { unoptimized: true },
+  basePath: "/Aigiri-Landing",
+  output: "export",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
